0f35aecf11a3c45ffb8eec10d7e4a9f1c6d97af0
Kevin Templeton <templeton.kevin@gmail.com>
2022-07-29 19:27:33 -0500

    step 2 complete

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from boggle import Boggle
 from flask import Flask, request, render_template, session
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
 boggle_game = Boggle()
 game_board = boggle_game.make_board()
 
