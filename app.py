2a19b17da6a0262a58f17d494281bf196d1a3a67
Kevin Templeton <templeton.kevin@gmail.com>
2022-07-29 19:26:48 -0500

    step 2 complete

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, session
 
 boggle_game = Boggle()
+game_board = boggle_game.make_board()
+
+@app.route('/')
+def homepage():
+    """Show the game board."""
+
+    return render_template('homepage.html', board=game_board)
